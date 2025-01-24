a13257a56e782381dfa491da4cb9071dbaaf7671
d-nt00 <mochiii00723@gmail.com>
2025-01-24 17:34:15 +0700

    add migration script v1 - init schema

---
+++ src/migrations/V20250124__init_schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE scheduling (
+    job_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    job_status varchar(30) NOT NULL,
+    job_details JSON NOT NULL,
+    scheduled_time TIMESTAMP NOT NULL
+);
