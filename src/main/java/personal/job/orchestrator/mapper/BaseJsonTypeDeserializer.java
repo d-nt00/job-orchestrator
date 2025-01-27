a1dc7d01962ee64c3b96bdf84ae0eb4d4e300c13
d-nt00 <mochiii00723@gmail.com>
2025-01-28 00:12:44 +0700

    add base json converters

---
+++ src/main/java/personal/job/orchestrator/mapper/BaseJsonTypeDeserializer.java
@@ -0,0 +1,27 @@
+package personal.job.orchestrator.mapper;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.r2dbc.postgresql.codec.Json;
+import lombok.AllArgsConstructor;
+import org.springframework.core.convert.converter.Converter;
+import personal.job.orchestrator.exception.ServiceException;
+
+import static personal.job.orchestrator.common.enums.CommonErrorMessages.JSON_PARSING_ERROR;
+
+@AllArgsConstructor
+public abstract class BaseJsonTypeDeserializer<T> implements Converter<Json, T> {
+
+    protected final ObjectMapper objectMapper;
+    protected Class<T> targetClass;
+
+    @Override
+    public T convert(Json source) {
+        try {
+            String jsonString = source.asString();
+            return objectMapper.readValue(jsonString, targetClass);
+        } catch (JsonProcessingException e) {
+            throw new ServiceException(JSON_PARSING_ERROR, e);
+        }
+    }
+}
---
+++ src/main/java/personal/job/orchestrator/mapper/BaseJsonTypeSerializer.java
@@ -0,0 +1,27 @@
+package personal.job.orchestrator.mapper;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.r2dbc.postgresql.codec.Json;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.convert.converter.Converter;
+import personal.job.orchestrator.exception.ServiceException;
+
+import static personal.job.orchestrator.common.enums.CommonErrorMessages.JSON_PARSING_ERROR;
+
+@RequiredArgsConstructor
+public abstract class BaseJsonTypeSerializer<F> implements Converter<F, Json> {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public Json convert(F source) {
+        try {
+            String rawJsonString = objectMapper.writer().withDefaultPrettyPrinter()
+                    .writeValueAsString(source);
+            return Json.of(rawJsonString);
+        } catch (JsonProcessingException e) {
+            throw new ServiceException(JSON_PARSING_ERROR, e);
+        }
+    }
+}
