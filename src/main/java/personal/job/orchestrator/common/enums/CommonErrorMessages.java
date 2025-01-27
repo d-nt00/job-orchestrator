f2a04d7c4beb6e55e1b2e7f4d713e4808f178f27
d-nt00 <mochiii00723@gmail.com>
2025-01-28 00:12:13 +0700

    add exception

---
+++ src/main/java/personal/job/orchestrator/common/enums/CommonErrorMessages.java
@@ -0,0 +1,6 @@
+package personal.job.orchestrator.common.enums;
+
+public final class CommonErrorMessages {
+
+    public static final String JSON_PARSING_ERROR = "Something went wrong while parsing JSON";
+}
