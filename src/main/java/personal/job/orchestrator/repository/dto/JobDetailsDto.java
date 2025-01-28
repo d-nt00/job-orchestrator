b735a7d9bd135fff41157bfb7c6c8f524bc19946
d-nt00 <mochiii00723@gmail.com>
2025-01-28 12:21:08 +0700

    update base json class

--- src/main/java/personal/job/orchestrator/repository/dto/JobDetailsDto.java
+++ src/main/java/personal/job/orchestrator/repository/dto/JobDetailsDto.java
@@ -1,20 +1,10 @@
 package personal.job.orchestrator.repository.dto;
 
-import com.fasterxml.jackson.annotation.JsonSubTypes;
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-@JsonTypeInfo(
-        use = JsonTypeInfo.Id.CLASS,
-        include = JsonTypeInfo.As.EXISTING_PROPERTY,
-        property = "eventName",
-        visible = true)
-@JsonSubTypes({
-        @JsonSubTypes.Type(value = NotificationTriggeredJobDetailsDto.class, name = "notification-triggered-system")
-})
 public abstract class JobDetailsDto {
     private String eventName;
 }
