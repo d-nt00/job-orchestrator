166c190e3eb214051a3c9bf8efe885346d6f905b
d-nt00 <mochiii00723@gmail.com>
2025-01-28 12:19:08 +0700

    add mixin for jsontype

---
+++ src/main/java/personal/job/orchestrator/config/JobDetailsDtoMixin.java
@@ -0,0 +1,16 @@
+package personal.job.orchestrator.config;
+
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import personal.job.orchestrator.repository.dto.NotificationTriggeredJobDetailsDto;
+
+@JsonTypeInfo(
+        use = JsonTypeInfo.Id.NAME,
+        include = JsonTypeInfo.As.EXISTING_PROPERTY,
+        property = "eventName",
+        visible = true)
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = NotificationTriggeredJobDetailsDto.class, name = "notification-triggered-system")
+})
+public abstract class JobDetailsDtoMixin {
+}
