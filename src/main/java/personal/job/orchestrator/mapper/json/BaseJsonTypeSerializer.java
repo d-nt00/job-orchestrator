41833ae43b035e83af4d1b8bf425a1bf6c71b1fb
d-nt00 <mochiii00723@gmail.com>
2025-01-31 17:51:53 +0700

    change directory

--- src/main/java/personal/job/orchestrator/mapper/json/BaseJsonTypeSerializer.java
+++ src/main/java/personal/job/orchestrator/mapper/json/BaseJsonTypeSerializer.java
@@ -1,4 +1,4 @@
-package personal.job.orchestrator.mapper;
+package personal.job.orchestrator.mapper.json;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
