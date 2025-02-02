8b7456522b526b4c918f9370cbe0a7916b535cc9
d-nt00 <mochiii00723@gmail.com>
2025-02-02 11:47:02 +0700

    add checkpoint

--- src/test/java/component/personal/job/orchestrator/repository/JobRepositoryTest.java
+++ src/test/java/component/personal/job/orchestrator/repository/JobRepositoryTest.java
@@ -59,6 +59,9 @@ public class JobRepositoryTest {
           assertNotNull(foundJob);
           assertNotNull(foundJob.jobId());
           assertEquals(dummyJob.jobStatus(), foundJob.jobStatus());
+          System.out.println(dummyJob.scheduledTime());
+          System.out.println(foundJob.scheduledTime());
+
           assertEquals(dummyJob.scheduledTime(), foundJob.scheduledTime());
           assertThat(dummyJob.jobDetails()).usingRecursiveComparison().isEqualTo(foundJob.jobDetails());
         })
